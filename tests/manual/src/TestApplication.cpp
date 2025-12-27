487dae678d68b7ef1a53eca3caf91b902d22d26c
Valentin Gerard <valentin.gerard17@hotmail.fr>
2025-12-27 19:52:19 +0100

    Clean TestApplication.cpp file

--- tests/manual/src/TestApplication.cpp
+++ tests/manual/src/TestApplication.cpp
@@ -21,15 +21,13 @@ void novum_engine::testing::TestApplication::onInit() const noexcept
 
     ImGui::StyleColorsDark();
 
-    auto* window = static_cast<GLFWwindow*>(m_window->getNativeWindow());
+    auto *window = static_cast<GLFWwindow*>(m_window->getNativeWindow());
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init();
 }
 
-void novum_engine::testing::TestApplication::onUpdate() noexcept
-{
-}
+void novum_engine::testing::TestApplication::onUpdate() noexcept {}
 
 void novum_engine::testing::TestApplication::onUpdateUI() noexcept
 {
